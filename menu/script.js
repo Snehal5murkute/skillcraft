b9147302b17a8290bc627e34fc4ec25b6be05e78
Snehalmurkute_ <snehalmurkute5@gmail.com>
2025-06-28 19:46:03 +0530

    Add files via upload

---
+++ menu/script.js
@@ -0,0 +1,11 @@
+
+
+// window.addEventListener("scroll", function () {
+//     const navbar = document.querySelector(".navbar");
+//     if (window.scrollY > 100) {
+//       navbar.style.backgroundColor = "#333";
+//     } else {
+//       navbar.style.backgroundColor = "transparent";
+//     }
+//   });
+  
